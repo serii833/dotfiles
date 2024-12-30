b01c7ef7dff75716db0dd15a04f5de43eab253f4
serii833 <Sergei.Babski@gmail.com>
2024-12-30 11:09:14 +0300

    Update options.lua

--- msys2/nvim/nvim/lua/options.lua
+++ msys2/nvim/nvim/lua/options.lua
@@ -25,9 +25,18 @@ vim.opt.updatetime = 50
 -- vim.g.loaded_netrwPlugin = 1
 -- vim.g.loaded_netrw = 1
 
+
+vim.opt.clipboard = "unnamedplus"
+
 vim.g.mapleader = " "
 vim.keymap.set("n", "<C-s>", ":w<CR>")
 vim.keymap.set("i", "<C-s>", "<Esc>:w<CR>")
+
+-- do not overwrite clipboard on paste over selection
+vim.keymap.set('x', 'p', 'P')
+vim.keymap.set('n', '<C-v>', 'p')
+vim.keymap.set('i', '<C-v>', '<Esc>pA')
+
 
 vim.keymap.set("n", "<leader>p", ":r!pbpaste<CR>'.kJ")
 -- vim.keymap.set("i", "<leader>p", ":r!pbpaste<CR>'.kJ")
